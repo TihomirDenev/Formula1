08aa92494ad20622014fdbe1153be82e9c370b20
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2025-01-16 22:41:29 +0200

    create hof component

---
+++ src/app/pages/hof/hof.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hof',
+  imports: [],
+  templateUrl: './hof.component.html',
+  styleUrl: './hof.component.scss'
+})
+export class HofComponent {
+
+}

a277e4488dcbb4ce584710b55777090a1eff7c99
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2025-01-16 22:42:22 +0200

    create point system component

---
+++ src/app/pages/point-system/point-system.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-point-system',
+  imports: [],
+  templateUrl: './point-system.component.html',
+  styleUrl: './point-system.component.scss'
+})
+export class PointSystemComponent {
+
+}

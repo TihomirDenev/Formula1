85f8d6c2e81d6f1b7a3ec947282b86b04286a869
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2025-01-18 17:34:01 +0200

    create point system with responsive design

--- src/app/pages/point-system/point-system.component.ts
+++ src/app/pages/point-system/point-system.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PointsSystem } from '../../interfaces';
+import { POINT_SYSTEM, POINT_SYSTEM_HEADER } from './point-system.data';
 
 @Component({
   selector: 'app-point-system',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './point-system.component.html',
-  styleUrl: './point-system.component.scss'
+  styleUrl: './point-system.component.scss',
 })
 export class PointSystemComponent {
-
+  pointsSystems: PointsSystem[] = POINT_SYSTEM;
+  pointSystemHeader: PointsSystem = POINT_SYSTEM_HEADER;
 }

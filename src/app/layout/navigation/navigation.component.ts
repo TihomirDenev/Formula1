d220f3f7ec1b90e94f7a46aed26a8c439eee0f2f
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2025-01-19 17:03:21 +0200

    Install translate module

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navigation',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  constructor(private translateService: TranslateService) {}
+
+  translateText(lang: string) {
+    this.translateService.use(lang);
+  }
+}

7a433f3a8425c272e97c68d59425018798e21240
Tihomir Denev <tdenev22@abv.bg>
2025-02-05 17:27:20 +0200

    add contact form

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contacts',
-  imports: [TranslateModule],
+  imports: [TranslateModule, ReactiveFormsModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss',
 })
-export class ContactsComponent {}
+export class ContactsComponent implements OnInit {
+  contactForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createContactForm();
+  }
+
+  createContactForm(): void {
+    this.contactForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      alert(
+        `Thank you, ${this.contactForm.value.firstName}! Your form has been submitted.`
+      );
+      this.contactForm.reset();
+    }
+  }
+}

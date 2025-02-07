615f8fbc3eddb1805de8942f35b45c52179d69eb
Tihomir Denev <156659230+TihomirDenev@users.noreply.github.com>
2025-02-08 01:16:32 +0200

    update photos component

--- src/app/pages/photos/photos.component.ts
+++ src/app/pages/photos/photos.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
 
+import { TranslateModule } from '@ngx-translate/core';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 @Component({
